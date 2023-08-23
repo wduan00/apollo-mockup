105c8ceb80058859f5c5e7406ad6d2f7fc016700
William Duan <wduan@ucsd.edu>
2023-08-22 21:01:25 -0700

    remove db connection, minor format changes

--- backend/server.js
+++ backend/server.js
@@ -12,28 +12,18 @@ const apolloURI = process.env.APOLLO_URI;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGO_URI;
-// open MongoDB connection prior to running the server
-mongoose
-	.connect(uri)
-	.then(() => {
-		// only run app if DB is connected
-		app.listen(port, () => {
-			console.log("Apollo app is running on port 5000");
-		});
-		console.log("connected to MongoDB");
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+app.listen(port, () => {
+	console.log(`Apollo app is running on port ${port}`);
+});
 
 // route handling
 app.get("/api", async (req, res) => {
 	try {
 		const response = await axios.post(apolloURI, {
-			person_titles: req.body?.person_titles,
-			q_organizion_domains: req.body?.q_organizion_domains,
-			page: req.body?.page,
+			api_key: process.env.APOLLO_API_KEY,
+			person_titles: req.query?.person_titles,
+			q_organizion_domains: req.query?.q_organizion_domains,
+			page: req.query?.page,
 		});
 		console.log("Apollo API call success");
 		res.status(200).send(response.data);
